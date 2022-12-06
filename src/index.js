3484610697948106b30e0603a22ea790bc009193
victor <victor.buthia@student.moringaschool.com>
2022-12-06 11:50:24 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const text = document.getElementById("new-task-description");
+  const submit = document.querySelector("input[type=submit]");
+  const ul = document.getElementById("tasks");
+  let totalList = [];
+
+  //eventlistener
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newLi = document.createElement("li"); 
+    const button = document.createElement("button"); 
+    button.innerHTML = "X"; 
+    button.addEventListener("click", () => {
+     
+      newLi.remove();
+    });
+    newLi.innerHTML = text.value; 
+    newLi.appendChild(button); 
+    ul.appendChild(newLi); 
+    totalList.push(newLi); 
+    console.log(totalList); 
+    text.value = ""; 
+  });
 });
+
+
